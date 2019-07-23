3f2f71e38b390c40983f91dff5a52faa326b4de7
Pablo Mansilla Ojeda <pablo.mansilla@ibm.com>
2019-07-23 01:13:48 +0100

    Install apiconnect@1.0.3

---
+++ docker/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "$ACCEPT_LICENSE" | grep -E '[Yy]([Ee][Ss])?' > /dev/null
+if [ $? -ne 0 ]
+then
+	printf "Do you must accept the license to use the Toolkit, set ACCEPT_LICENSE=y\n"
+	exit 1
+fi
+
+apic --accept-license > /dev/null
+apic edit
