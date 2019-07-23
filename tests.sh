86d590712798e925b2f8e224e875a0d979dc25d2
Pablo Mansilla Ojeda <pablo.mansilla@ibm.com>
2019-07-23 05:41:49 +0100

    Add a check for server pid

--- tests.sh
+++ tests.sh
@@ -12,9 +12,18 @@ fi
 printf -- $GREEN"* Running tests:\n"$RESET
 printf -- $YELLOW"+ Preparing environment .. ${GREEN}OK\n"$RESET
 apic --accept-license > /dev/null
+printf -- $YELLOW"+ Starting $BLUEAPI Connect Developer Toolkit ."$RESET
 SKIP_LOGIN=true apic edit > /.apic.log &
 APIC_PID=$!
-printf -- $YELLOW"+ Starting $BLUEAPI Connect Developer Toolkit .. ${GREEN}OK\n"$RESET
+ps -a | grep $APIC_PID > /dev/null
+if [ $? -eq 0 ]
+then
+	printf -- "$YELLOW. ${GREEN}OK\n"$RESET
+else
+	printf -- "$YELLOW. ${RED}NOK\n"$RESET
+	exit 1
+fi
+
 printf -- $YELLOW"+ Waiting for server to start at port$BLUE 9090$YELLOW ."$RESET
 FOUND=0
 COUNTER=0
